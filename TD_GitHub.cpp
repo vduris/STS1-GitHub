a5844b2349702ea2a6ad86f09d9d5ba7852447db
vduris <80945899+vduris@users.noreply.github.com>
2021-04-05 20:42:05 +0200

    Fonction étudiant 1

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,14 +1,57 @@
 //Fonctions réalisées par l'étudiant 1 :
+bool estMajeur(int age)
+{
+        bool resultat;
 
+        if (age >= 18)
+        {
+                resultat = true;
+        }
+        else if(age<18)
+        {
+                resultat = false;
+        }
+        return resultat;
+}
 
+int exposant(int nbre, int exp)
+{
+        int resultat;
 
+        resultat = pow(nbre, exp);
 
+        return resultat;
 
+}
 
+float TVA(int prix)
+{
+        float resultat;
 
+        resultat = prix * 1.196;
 
+        return resultat;
 
+}
 
+int plusGrand(int a, int b, int c)
+{
+        int resultat=0;
+
+        if (a > b && a > c)
+        {
+                resultat = a;
+        }
+        else if (b > c && b > a)
+        {
+                resultat = b;
+        }
+        else if (c > a && c > b)
+        {
+                resultat = c;
+        }
+        return resultat;
+}
 //Fonctions réalisées par l'étudiant 2 :
 
 
